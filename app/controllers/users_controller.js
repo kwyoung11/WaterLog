2a794e7b40469cd24e7477b2bcdc58e324693a82
Kevin Young <kyoung18@terpmail.umd.edu>
2015-10-21 17:07:52 -0400

    authentication

--- app/controllers/users_controller.js
+++ app/controllers/users_controller.js
@@ -2,14 +2,19 @@
 * This controller will handle everything related to user management.
 */
 var view = require('../../lib/view');
+var db = require('../../lib/db');
+var bcrypt = require('bcrypt');
+var crypto = require('crypto');
+var util = require('../../lib/util');
 var users_controller = function() {};
 users_controller.prototype = {
 	
 	// GET /users/new
 	new: function(params, callback) {
 		var callback = (typeof callback === 'function') ? callback : function() {};
-		
-		var data = null;
+		data = null;
+		console.log("THIS IS THE REQUEST: " + req);
+		// respond with login/registration page
 		view.renderView('users/new', data, function(data) {
 		  callback(data);
 		});
@@ -21,7 +26,7 @@ users_controller.prototype = {
 		
 		// load user data here
 		var data = null;
-		view.renderView('users/new', data, function(data) {
+		view.renderView('users/show', data, function(data) {
 		  callback(data);
 		});
 	},
@@ -33,6 +38,31 @@ users_controller.prototype = {
 
 	// POST /users
 	create: function(params, callback) {
+		var toby = new User(params);
+		// create the users authentication token, which will be stored in a cookie
+		var token = crypto.randomBytes(64).toString('hex');
+		
+		// generate a salt, then encrypt the inputted password with that salt
+		bcrypt.genSalt(10, function(err, salt) {
+    	bcrypt.hash('B4c0/\/', salt, function(err, hash) {
+
+    	    // create a new user in the database, storing the hashed password along with the salt
+					db.query("INSERT INTO users (email, password_digest, auth_token, salt) VALUES ($1, $2, $3, $4)", [params['email'], hash, token, salt], function(err, result) {
+
+						// the user has now succesfully registered, lets initialize his cookie
+						util.cookie = token;
+						data = result.rows[0];
+						view.renderView('users/show', data, function(data) {
+						  callback(data);
+						});				
+					});
+    	});
+		});
+
+		
+		// set the users auth token in a cookie
+
+		// respond with User Profile page
 
 	},
 
