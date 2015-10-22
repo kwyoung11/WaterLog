a4aaf645e8d7ef46360cfa4989bd5ac8b0425bee
crschroeder94 <crschroeder94@hotmail.com>
2015-10-22 15:54:59 -0400

    added functionality

--- app/controllers/devices_controller.js
+++ app/controllers/devices_controller.js
@@ -6,6 +6,55 @@ var view = require('../../lib/view');
 var devices_controller = function() {};
 devices_controller.prototype = {
 
+	//GET /devices/new
+	add: function(params, callback) {
+		var callback = (typeof callback === 'function') ? callback : function() {};
+		
+		var data = null;
+		view.renderView('/devices/new', data, function(data) {
+		  callback(data);
+		});
+	},
+
+	//GET /devices/edit
+
+	view: function(params, callback) {
+			var callback = (typeof callback === 'function') ? callback : function() {};
+			
+			var data = {
+			  'devices' : [{
+			    'nickname'    : 'Device1',
+			    'viewLink': 'devices/Device1/',
+			    'ID Number' : '1234'
+			  	},{
+			    'nickname'    : 'Device2',
+			    'viewLink': 'devices/Device2/',
+			    'ID Number' : '1234'
+			  	}]
+			};
+			
+			view.renderView('/devices/view', data, function(data) {
+			  callback(data);
+			});
+	},
+	update: function(params, callback) {
+		var callback = (typeof callback === 'function') ? callback : function() {};
+		
+		var data = null;
+		view.renderView('/devices/new', data, function(data) {
+		  callback(data);
+		});
+	},
+	destroy: function(params, callback) {
+		var callback = (typeof callback === 'function') ? callback : function() {};
+		
+		var data = null;
+		view.renderView('/devices/new', data, function(data) {
+		  callback(data);
+		});
+	}
+
+
 }
 
 module.exports = new devices_controller();
