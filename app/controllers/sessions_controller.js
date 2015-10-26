7a5082d57c7ca218f9b46079cc292562b765bdc6
Kevin Young <kyoung18@terpmail.umd.edu>
2015-10-26 02:25:54 -0400

    login#new

--- app/controllers/sessions_controller.js
+++ app/controllers/sessions_controller.js
@@ -3,17 +3,29 @@ This controller will handle users sessions.
 */
 
 var view = require('../../lib/view');
-var sessions_controller = function() {};
+var sessions_controller = function(response_handler) {
+	this.response_handler = response_handler;
+};
+
 sessions_controller.prototype = {
 
+	// login page
+	new: function(params, cb) {
+		view.renderView("sessions/new", null, function(content) {
+			cb(content);
+		}); 
+	},
+
+	// on login submit
 	create: function(params, cb) {
 		
 	},
 
+	// /logout
 	destroy: function(params, cb) {
 		
 	}
 
 };
 
-module.exports = new sessions_controller();
+module.exports = sessions_controller;
