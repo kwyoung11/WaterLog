a2b496215587ae072683d7e410c6573a926d9f9d
Kevin Young <kyoung18@terpmail.umd.edu>
2015-10-28 20:13:08 -0400

    password resets functionality; wrote a mailer class; implemented a user.update(); fixed asset loading in webroot/

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,6 +3,7 @@ exports.devices = require("./devices_controller.js");
 exports.home = require("./home_controller.js");
 exports.data = require("./data_controller.js");
 exports.sessions = require("./sessions_controller.js");
+exports.password_resets = require("./password_resets_controller.js");
 
 // module.exports.users = {
 // 	'users': require("./users_controller.js"),
