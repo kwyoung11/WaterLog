1ba0cfae11428981db0c8ccbda579e87e6fef8e2
Kevin Young <kyoung18@terpmail.umd.edu>
2015-11-05 01:15:50 -0500

    GET api complete, updated package.json dependencies

--- app/controllers/api_controller.js
+++ app/controllers/api_controller.js
@@ -27,22 +27,47 @@ api_controller.prototype = {
 	
 	device: function (params, cb) {
 		var self = this;
-		if (params.location && params.radius) {
-			console.log("in clause");
+		if (params.location && params.radius) { // get devices with x mile radius of lat, lon
 			var latitude = params.location.split(",")[0];
 			var longitude = params.location.split(",")[1];
-			console.log(latitude);
 			var top = latitude - (params.radius/69) + (2*(params.radius/69));
 			var bot = latitude - (params.radius/69);
 			var right = longitude - (params.radius/54) + (2*(params.radius/54));
 			var left = longitude - (params.radius/54);
-			console.log(latitude);
-			console.log(longitude);
-			console.log(top);
-			console.log(bot);
-			console.log(right);
-			console.log(left);
 			db.query("SELECT * from devices WHERE longitude BETWEEN $1 AND $2 AND latitude BETWEEN $3 AND $4", [right, left, bot, top], function(err, result) {
+				if (err) {
+					console.log(err);
+					return cb(err);
+				}
+				console.log(result);
+				return self.response_handler.renderJSON(200, result.rows);
+			});
+
+		// get all data records for device	
+		} else if (!params.start_time && !params.latest_record) { 
+			db.query("SELECT * from data WHERE device_id = $1", [params.device_id], function(err, result) {
+				if (err) {
+					console.log(err);
+					return cb(err);
+				}
+				console.log(result);
+				return self.response_handler.renderJSON(200, result.rows);
+			});
+
+		// get all data records for device within specified time period	
+		} else if (params.start_time && params.end_time) { 
+			db.query("SELECT * from data WHERE device_id = $1 AND created_at BETWEEN $2 AND $3", [params.device_id, params.start_time, params.end_time], function(err, result) {
+				if (err) {
+					console.log(err);
+					return cb(err);
+				}
+				console.log(result);
+				return self.response_handler.renderJSON(200, result.rows);
+			});
+
+		// get latest record for device
+		} else if (params.latest_record) {
+			db.query("SELECT * from data WHERE device_id = $1 ORDER BY created_at DESC LIMIT 1", [params.device_id], function(err, result) {
 				if (err) {
 					console.log(err);
 					return cb(err);
@@ -54,7 +79,49 @@ api_controller.prototype = {
 	},
 
 	users: function(params, cb) {
+		// gets all user device info as well as user info
+		/* example JSON: 
+		{
+		  "user": {
+		        "email": "myEmail@email.com"
+		  },
+		  "devices": [
+		        {
+		            "id": 1,
+		            "name": "myDevice",
+		            "records": [
+		                {
+		                    "pH": 8,
+		                    "temperature": 70,
+		                    "timestamp": "10/28/15"
+		                }
+		            ]
+		        }
+		    ]
+		}
+		********************************
+		*/
+		var self = this;
+		if (params.user_id) {
+			db.query("SELECT id, email FROM users WHERE id = $1", [params.user_id], function(err, user_result) {
+				if (err) {
+					console.log(err);
+					return cb(err);
+				}
+				db.query("SELECT * FROM devices WHERE user_id = $1", [params.user_id], function(err, devices_result) {
+				if (err) {
+					console.log(err);
+					return cb(err);
+				}
 
+				var json = {};
+				json["user"] = user_result.rows[0];
+				json["devices"] = devices_result.rows;
+				console.log(json);
+				return self.response_handler.renderJSON(200, json);
+			});	
+			});	
+		}  
 	}
 
 };
