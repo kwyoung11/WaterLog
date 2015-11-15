69d3382c7a7d70e9aa3b32671040b69173197e8e
Kevin Young <kyoung18@terpmail.umd.edu>
2015-11-15 03:14:14 -0500

    installation script

--- seeds.js
+++ seeds.js
@@ -1,7 +1,4 @@
-var db = require('lib/db');
-var User = require('app/models/user');
-var Device = require('app/models/device'); 
-var Data = require('/app/models/data');
+var db = require('./lib/db');
 var moment = require('moment');
 
 var a = moment('2015-06-01'); // July 1 2015
@@ -13,17 +10,14 @@ for (var m = a; m.isBefore(b); m.add('days', 1)) {
 	var temp = Math.random() * (80 - 10) + 10;
 	var datetime = m.format();
   console.log(datetime);
-  db.query("INSERT INTO data (device_id, data_type, keys, values, created_at) VALUES (1, 'Water', '{pH, turbidity, temperature}', '{$1, $2, $3}', NOW())", [pH, tds, temp], function(err, result) {
+  db.query("INSERT INTO data (device_id, data_type, keys, values, created_at) VALUES (1, 'Water', '{pH, turbidity, temperature}', '{" + ph + "," + tds + "," + temp + "}', '"+datetime+"')", [], function(err, result) {
 
   });
 
   var ph = Math.random() * (9.0 - 5.0) + 5.0;
 	var tds = Math.random() * (400);
 	var temp = Math.random() * (80 - 10) + 10;
-	db.query("INSERT INTO data (device_id, data_type, keys, values, created_at) VALUES (2, 'Water', '{pH, turbidity, temperature}', '{$1, $2, $3}', NOW())", [pH, tds, temp], function(err, result) {
+	db.query("INSERT INTO data (device_id, data_type, keys, values, created_at) VALUES (2, 'Water', '{pH, turbidity, temperature}', '{" + ph + "," + tds + "," + temp + "}', '"+datetime+"')", [], function(err, result) {
 
   });	
 }
-	
-
-	
