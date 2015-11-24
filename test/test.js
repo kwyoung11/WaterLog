6ff4d48b9cd8d8601e2821188a81a051e8de911f
Byedinak <bcyedinak@gmail.com>
2015-11-23 20:27:28 -0500

    Added test for unencrypted data posts

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var User = require('../app/models/user');
 var Device = require('../app/models/device');
 
 
-function runPostTest(){
+function runPostTest(encryptedOption, cb){
 	var path = 'http://127.0.0.1:3000/data/newData?';
 	
 	var user_gen_params = {'email' : 'fakeEmail' + User.generateKeyPair().private,
@@ -31,25 +31,30 @@ function runPostTest(){
 					
 					// a set of possible post parameters
 					var data = 'data_type=water&pH=0&latitude=5&longitude=5&device_id=' +  deviceAfterInsert.id;
-					var algorithm = 'aes-128-cbc';
-					var clearEncoding = 'utf8';
-					var cipherEncoding = 'hex';
 					
-					//encrypt data with shared private key
 					
-					var cipher = crypto.createCipher(algorithm, userAfterInsert.shared_private_key);
-					var encData = cipher.update(data, clearEncoding, cipherEncoding);
-					
-					// encrypted data
-					encData += cipher.final(cipherEncoding);
-					
-					// sign encrypted data with private key
-					var sign = crypto.createSign('RSA-SHA256');
-					sign.update(encData);
-					var signedData = sign.sign(userAfterInsert.private_key, 'hex');
-					data = 'signedData=' + signedData + '&unsignedData=' + encData + '&device_id=' + deviceAfterInsert.id;
-					console.log('signed data is');
-					console.log(signedData);
+					if(encryptedOption == true){
+						
+						var algorithm = 'aes-128-cbc';
+						var clearEncoding = 'utf8';
+						var cipherEncoding = 'hex';
+						
+						//encrypt data with shared private key
+						
+						var cipher = crypto.createCipher(algorithm, userAfterInsert.shared_private_key);
+						var encData = cipher.update(data, clearEncoding, cipherEncoding);
+						
+						// encrypted data
+						encData += cipher.final(cipherEncoding);
+						
+						// sign encrypted data with private key
+						var sign = crypto.createSign('RSA-SHA256');
+						sign.update(encData);
+						var signedData = sign.sign(userAfterInsert.private_key, 'hex');
+						data = 'signedData=' + signedData + '&unsignedData=' + encData + '&device_id=' + deviceAfterInsert.id;
+						
+						
+					}
 					
 					//send post request
 					var options = {
@@ -61,7 +66,6 @@ function runPostTest(){
 					  method: 'post'
 					};
 					
-					
 					request(options, function(err, response, body){
 						if(err){
 							console.log(err);
@@ -69,10 +73,8 @@ function runPostTest(){
 						else{
 							console.log(response);
 						}
-						
-						process.exit();
+						cb();
 					});
-					
 				}
 			);
 			
@@ -80,5 +82,7 @@ function runPostTest(){
 		
     
 }
-
-runPostTest();
+// test for encrypted posts.  We should see something like "Post successful" in the response body
+runPostTest(true, function(){});
+// test for unencrypted posts.  We should see something like "Unencrypted post successful" in the response body
+runPostTest(false, function(){process.exit();});
