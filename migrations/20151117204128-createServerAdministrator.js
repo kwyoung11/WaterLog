62f50637001987fc852060e87769ccd451b098c1
Kevin Young <kyoung18@terpmail.umd.edu>
2015-12-03 11:43:22 -0500

    fixing hanging migration

--- migrations/20151117204128-createServerAdministrator.js
+++ migrations/20151117204128-createServerAdministrator.js
@@ -15,13 +15,8 @@ exports.up = function(db, callback) {
 	var params = {'email' : 'admin@admin.com',
 					'password_digest' : 'admin',
 					'is_admin' : true};
-		var user = new User(params, {'sync': true});
-		var arr = [user.data.email, user.data.password_digest, user.data.auth_token, user.data.salt, user.data.public_key, user.data.private_key, user.data.shared_private_key, user.data.email_confirmation_token];
-		var string = arr.join("','");
-		string = "'" + string + "'";
-		// console.log(string);
-		db.runSql('INSERT INTO users (email, password_digest, auth_token, salt, public_key, private_key, shared_private_key, email_confirmation_token) VALUES (' + string +')', [], callback);
-		// user.save(function(err, user) {throw true;});
+		var user = new User(params);
+		user.save(function(err, user) {throw true;});
 };
 
 exports.down = function(db, callback) {
