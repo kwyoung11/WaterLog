01c566b23e0db3e8dbe804651d6bfc7b4209b74d
Kevin Young <kyoung18@terpmail.umd.edu>
2015-12-08 16:53:06 -0500

    updates

--- migrations/20151208214034-addOffsetsToDevices.js
+++ migrations/20151208214034-addOffsetsToDevices.js
@@ -2,7 +2,7 @@ var dbm = global.dbm || require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  db.runSql('ALTER TABLE "devices" ADD COLUMN offsets real[]',[], callback);
+  db.runSql("ALTER TABLE devices ADD COLUMN offsets real[] DEFAULT '{0.0, 0.0, 0.0, 0.0, 0.0}'",[], callback);
 };
 
 exports.down = function(db, callback) {
